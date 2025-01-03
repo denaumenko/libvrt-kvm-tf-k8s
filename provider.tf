1be9325127776d47cc7260dc0bdc18762c9320b2
Denys Naumenko <denaumenko@gmail.com>
2025-01-03 10:49:26 +0100

    Rename description for Ansible Playbook

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    libvirt = {
-      source  = "dmacvicar/libvirt"
-      version = "0.8.1"
-    }
-  }
-}
-
 provider "libvirt" {
   uri = "qemu:///system"
 }
